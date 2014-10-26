a124f20a4b2b5f3f0aa5befaac6fe8a4062bc002
laiy <ly.franky@gmail.com>
2014-10-26 11:35:49 +0800

    Committing fixes in the dark, seriously, who killed my power!?

--- w4-5-7.cpp
+++ w4-5-7.cpp
@@ -1,24 +1,41 @@
-#include <iostream>
+#include <stdio.h>
 
-int findNumberOfWays(int m, int n) {
-    if (m == 1 || n == 1) {
-        return 1;
-    } else if (n >= m) {
-        return 1 + findNumberOfWays(m, m - 1);
+int list[100];
+
+void print(int index, int m) {
+    if (index > m) return;
+    int i;
+    for (i = 0; i <= index - 1; i++) {
+        printf("%d", list[i]);
+    }
+    for (i = index; i < m; i++) {
+        printf("0");
+    }
+    printf("\n");
+}
+
+void split(int n, int index, int m) {
+    int i;
+    if (n == 0) {
+        print(index, m);
     } else {
-        return findNumberOfWays(m - n, n) + findNumberOfWays(m, n - 1);
+        for (i = n; i >= 1; i--) {
+            if (index == 0 || i <= list[index - 1]) {
+                list[index] = i;
+                split(n - i, index + 1, m);
+            }
+        }
     }
 }
 
 int main() {
     int t;
-    std::cin >> t;
+    scanf("%d", &t);
     while (t--) {
-        int m, n;
-        std::cin >> m >> n;
-        std::cout << findNumberOfWays(m, n) << std::endl;
+        int n, m;
+        scanf("%d%d", &n, &m);
+        split(n, 0, m);
     }
     return 0;
 }
-
 
