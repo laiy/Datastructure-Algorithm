8ccd5b56d759f44dcab54db533b54056424c9fa5
laiy <ly.franky@gmail.com>
2014-11-03 10:17:21 +0800

    changes

--- extraExercises/1025.cpp
+++ extraExercises/1025.cpp
@@ -4,15 +4,40 @@
 #include <queue>
 
 std::vector<int> v[101];
-int n, m;
+int n, m, count = 0;
 bool visited[101];
-int color[101];
+bool searched[101];
 std::queue<int> q;
 
-bool bfs() {
-    memset(color, 0, sizeof(color));
-    memset(visited, false, sizeof(visited));
-    color[1] = 1;
+void search() {
+    memset(searched, false, sizeof(searched));
+    q.push(1);
+    while (!q.empty()) {
+        memset(visited, false, sizeof(visited));
+        int temp = q.front();
+        q.pop();
+        visited[temp] = true;
+        searched[temp] = true;
+        std::queue<int> temp_queue;
+        temp_queue.push(temp);
+        while (!temp_queue.empty()) {
+            int x = temp_queue.front();
+            temp_queue.pop();
+            for (int i = 0; i < v[x].size(); i++) {
+                if (visited[v[x][i]]) continue;
+                visited[v[x][i]] = true;
+                searched[v[x][i]] = true;
+                temp_queue.push(v[x][i]);
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!searched[i]) {
+                q.push(i);
+                break;
+            }
+        }
+        count++;
+    }
 }
 
 int main() {
@@ -23,11 +48,8 @@ int main() {
         v[a].push_back(b);
         v[b].push_back(a);
     }
-    if (bfs()) {
-        printf("yes\n");
-    } else {
-        printf("no\n");
-    }
+    search();
+    printf("%d\n", count);
     return 0;
 }
 
