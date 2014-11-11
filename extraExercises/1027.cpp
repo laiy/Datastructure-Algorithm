d3e3c903a51b0ff72ce0ca36b52042b9b6baf366
laiy <ly.franky@gmail.com>
2014-11-11 15:08:06 +0800

    It's secret!

--- extraExercises/1027.cpp
+++ extraExercises/1027.cpp
@@ -1,50 +1,51 @@
 #include <cstdio>
-#include <vector>
 #include <algorithm>
-#include <queue>
+#include <deque>
 
 struct HUFFMAN_TREE_NODE {
     int weight, code_length;
     HUFFMAN_TREE_NODE *left, *right;
 };
 
-std::vector<HUFFMAN_TREE_NODE> container;
+int n;
 
-int cmp(HUFFMAN_TREE_NODE a, HUFFMAN_TREE_NODE b) {
-    return a.weight < b.weight;
+std::deque<HUFFMAN_TREE_NODE*> forest;
+
+int cmp(HUFFMAN_TREE_NODE *a, HUFFMAN_TREE_NODE *b) {
+    return a->weight < b->weight;
 }
 
 void create_huffman_tree() {
-    while (container.size() != 1) {
+    for (int i = 0; i < n - 1; i++) {
+        std::sort(forest.begin(), forest.end(), cmp);
         HUFFMAN_TREE_NODE *new_node = new HUFFMAN_TREE_NODE;
-        std::vector<HUFFMAN_TREE_NODE>::iterator it = container.begin();
-        new_node->weight = container[0].weight + container[1].weight;
-        new_node->left = &container[0];
-        new_node->right = &container[1];
-        container.erase(it + 1);
-        container.erase(it);
-        container.insert(container.begin(), *new_node);
+        new_node->weight = forest[0]->weight + forest[1]->weight;
+        new_node->left = forest[0];
+        new_node->right = forest[1];
+        forest.pop_front();
+        forest.pop_front();
+        forest.push_back(new_node);
     }
 }
 
 void search_tree(HUFFMAN_TREE_NODE* node) {
     int counter = 0;
-    std::queue<HUFFMAN_TREE_NODE*> q;
+    std::deque<HUFFMAN_TREE_NODE*> q;
     node->code_length = 0;
-    q.push(node);
+    q.push_back(node);
     while (!q.empty()) {
         HUFFMAN_TREE_NODE *temp = q.front();
-        q.pop();
+        q.pop_front();
         if (temp->left == NULL && temp->right == NULL) {
             counter += temp->code_length * temp->weight;
         } else {
             if (temp->left != NULL) {
                 temp->left->code_length = temp->code_length + 1;
-                q.push(temp->left);
+                q.push_back(temp->left);
             }
             if (temp->right != NULL) {
                 temp->right->code_length = temp->code_length + 1;
-                q.push(temp->right);
+                q.push_back(temp->right);
             }
         }
     }
@@ -52,7 +53,7 @@ void search_tree(HUFFMAN_TREE_NODE* node) {
 }
 
 int main() {
-    int n, weight;
+    int weight;
     char c[2];
     scanf("%d", &n);
     if (!n) return 0;
@@ -61,11 +62,10 @@ int main() {
         HUFFMAN_TREE_NODE *new_node = new HUFFMAN_TREE_NODE;
         new_node->weight = weight;
         new_node->left = new_node->right = NULL;
-        container.push_back(*new_node);
+        forest.push_back(new_node);
     }
-    std::sort(container.begin(), container.end(), cmp);
     create_huffman_tree();
-    search_tree(&container[0]);
+    search_tree(forest.front());
     return 0;
 }
 
