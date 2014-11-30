16b29867b8fb7f116c50d29c49fdc80717344fe6
laiy <ly.franky@gmail.com>
2014-11-30 10:43:35 +0800

    oh fucking memset!

--- w9-5-2.cpp
+++ w9-5-2.cpp
@@ -13,39 +13,41 @@ int m_array[101];
 int n_array[101];
 bool m_visited[80000];
 bool n_visited[80000];
-bool m_negative[80000];
-bool n_negative[80000];
+bool m_negative_visited[80000];
+bool n_negative_visited[80000];
 
 void print_intersection() {
     for (int i = 0; i < m; i++)
-        m_array[i] >= 0 ? n_visited[m_array[i]] ? printf("%d ", m_array[i]) : 0 : n_negative[-m_array[i]] ? printf("%d ", m_array[i]) : 0;
+        m_array[i] >= 0 ? n_visited[m_array[i]] ? printf("%d ", m_array[i]) : 0 : n_negative_visited[-m_array[i]] ? printf("%d ", m_array[i]) : 0;
     printf("\n");
 }
 
 void print_union() {
     for (int i = 0; i < m; i++) printf("%d ", m_array[i]);
     for (int i = 0; i < n; i++)
-        n_array[i] >= 0 ? !m_visited[n_array[i]] ? printf("%d ", n_array[i]) : 0 : !m_negative[-n_array[i]] ? printf("%d ", n_array[i]) : 0;
+        n_array[i] >= 0 ? !m_visited[n_array[i]] ? printf("%d ", n_array[i]) : 0 : !m_negative_visited[-n_array[i]] ? printf("%d ", n_array[i]) : 0;
     printf("\n");
 }
 
 void print_different_set() {
     for (int i = 0; i < m; i++)
-        m_array[i] >= 0 ? !n_visited[m_array[i]] ? printf("%d ", m_array[i]) : 0 : !n_negative[-m_array[i]] ? printf("%d ", m_array[i]) : 0;
+        m_array[i] >= 0 ? !n_visited[m_array[i]] ? printf("%d ", m_array[i]) : 0 : !n_negative_visited[-m_array[i]] ? printf("%d ", m_array[i]) : 0;
     printf("\n");
+}
+
+void clear_visited() {
+        for (int i = 0; i < m; i++) m_array[i] >= 0 ? m_visited[m_array[i]] = false : m_negative_visited[-m_array[i]] = false;
+        for (int i = 0; i < n; i++) n_array[i] >= 0 ? n_visited[n_array[i]] = false : n_negative_visited[-n_array[i]] = false;
 }
 
 int main() {
     while (scanf("%d%d", &m, &n) != EOF) {
-        memset(m_visited, false, sizeof(bool));
-        memset(n_visited, false, sizeof(bool));
-        memset(m_negative, false, sizeof(bool));
-        memset(n_negative, false, sizeof(bool));
-        for (int i = 0; i < m; i++) scanf("%d", &m_array[i]), m_array[i] >= 0 ? m_visited[m_array[i]] = true : m_negative[-m_array[i]] = true;
-        for (int i = 0; i < n; i++) scanf("%d", &n_array[i]), n_array[i] >= 0 ? n_visited[n_array[i]] = true : n_negative[-n_array[i]] = true;
+        for (int i = 0; i < m; i++) scanf("%d", &m_array[i]), m_array[i] >= 0 ? m_visited[m_array[i]] = true : m_negative_visited[-m_array[i]] = true;
+        for (int i = 0; i < n; i++) scanf("%d", &n_array[i]), n_array[i] >= 0 ? n_visited[n_array[i]] = true : n_negative_visited[-n_array[i]] = true;
         print_intersection();
         print_union();
         print_different_set();
+        clear_visited();
     }
     return 0;
 }
