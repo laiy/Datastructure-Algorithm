52e542adc2e927e8c96eea3cecb1094130facfb2
laiy <ly.franky@gmail.com>
2014-11-30 10:17:03 +0800

    233

--- w9-5-2.cpp
+++ w9-5-2.cpp
@@ -13,23 +13,25 @@ int m_array[101];
 int n_array[101];
 bool m_visited[80000];
 bool n_visited[80000];
+bool m_negative[80000];
+bool n_negative[80000];
 
 void print_intersection() {
     for (int i = 0; i < m; i++)
-        if (n_visited[m_array[i]]) printf("%d ", m_array[i]);
+        m_array[i] >= 0 ? n_visited[m_array[i]] ? printf("%d ", m_array[i]) : 0 : n_negative[-m_array[i]] ? printf("%d ", m_array[i]) : 0;
     printf("\n");
 }
 
 void print_union() {
     for (int i = 0; i < m; i++) printf("%d ", m_array[i]);
     for (int i = 0; i < n; i++)
-        if (!m_visited[n_array[i]]) printf("%d ", n_array[i]);
+        n_array[i] >= 0 ? !m_visited[n_array[i]] ? printf("%d ", n_array[i]) : 0 : !m_negative[-n_array[i]] ? printf("%d ", n_array[i]) : 0;
     printf("\n");
 }
 
 void print_different_set() {
     for (int i = 0; i < m; i++)
-        if (!n_visited[m_array[i]]) printf("%d ", m_array[i]);
+        m_array[i] >= 0 ? !n_visited[m_array[i]] ? printf("%d ", m_array[i]) : 0 : !n_negative[-m_array[i]] ? printf("%d ", m_array[i]) : 0;
     printf("\n");
 }
 
@@ -37,8 +39,10 @@ int main() {
     while (scanf("%d%d", &m, &n) != EOF) {
         memset(m_visited, false, sizeof(bool));
         memset(n_visited, false, sizeof(bool));
-        for (int i = 0; i < m; i++) scanf("%d", &m_array[i]), m_visited[m_array[i]] = true;
-        for (int i = 0; i < n; i++) scanf("%d", &n_array[i]), n_visited[n_array[i]] = true;
+        memset(m_negative, false, sizeof(bool));
+        memset(n_negative, false, sizeof(bool));
+        for (int i = 0; i < m; i++) scanf("%d", &m_array[i]), m_array[i] >= 0 ? m_visited[m_array[i]] = true : m_negative[-m_array[i]] = true;
+        for (int i = 0; i < n; i++) scanf("%d", &n_array[i]), n_array[i] >= 0 ? n_visited[n_array[i]] = true : n_negative[-n_array[i]] = true;
         print_intersection();
         print_union();
         print_different_set();
