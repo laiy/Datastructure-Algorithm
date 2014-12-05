279e2321461e90895dc4928bd3233b585e0fbd0a
laiy <ly.franky@gmail.com>
2014-12-05 20:36:28 +0800

    Updated build targets.

--- w10-5-4.cpp
+++ w10-5-4.cpp
@@ -7,7 +7,20 @@
 
 #include <cstdio>
 
+int a, b, m, n;
+int table[301][11];
+
+void output() {
+    for (int i = 1; i <= a; i++) {
+        for (int j = 1; j <= b - 1; j++) printf("%d ", table[i][j]);
+        printf("%d\n", table[i][b]);
+    }
+}
+
 int main() {
+    scanf("%d%d", &a, &b);
+    while (scanf("%d%d", &m, &n) != EOF) table[m][n]++;
+    output();
     return 0;
 }
 
