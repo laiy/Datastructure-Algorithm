c1da2fe1d7bcfb4395b31a6a390b8d5771ea570a
laiy <ly.franky@gmail.com>
2014-12-28 20:40:11 +0800

    I am sorry

--- extraExercises/1009.cpp
+++ extraExercises/1009.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <cstdio>
 
 void QuickSort(int a[], int numsize) {
     int i = 0, j = numsize - 1;
@@ -28,19 +28,16 @@ int main() {
     int n;
     scanf("%d", &n);
     int signal[10];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         scanf("%d", &signal[i]);
-    }
     QuickSort(signal, n);
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < n - 1; i++)
         printf("%d ", signal[i]);
-    }
     printf("%d\n", signal[n - 1]);
-    if (n % 2 == 0) {
+    if (n % 2 == 0)
         printf("%d", (signal[n / 2 - 1] + signal[n / 2]) / 2);
-    } else {
+    else
         printf("%d", signal[n / 2]);
-    }
     return 0;
 }
 
