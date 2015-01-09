a0e7b5a852ff9074bff06d4a3c67c13879b2d27a
laiy <ly.franky@gmail.com>
2015-01-09 19:00:28 +0800

    add actual words

--- gjr/Weighted_Graph_Algorithms/1000.cpp
+++ gjr/Weighted_Graph_Algorithms/1000.cpp
@@ -13,20 +13,26 @@
 #define min(a, b) (a < b ? a : b)
 
 int how_many_roads, place_count;
-std::string places[101];
+std::string places[205];
 int distance[205][205];
 
 void record(std::string from, std::string to, int length) {
-    int first = 0;
+    int first = 0, second = 0;
+    bool got_first = false;
     for (int i = 1; i <= place_count; i++) {
         if (places[i] == from || places[i] == to) {
-            first = i;
-            break;
+            if (!got_first) first = i;
+            else second = i;
+            got_first = true;
         }
     }
     if (first) {
-        places[++place_count] = (places[first] == from ? to : from);
-        distance[first][place_count] = distance[place_count][first] = length;
+        if (second) {
+            distance[first][second] = distance[second][first] = length;
+        } else {
+            places[++place_count] = (places[first] == from ? to : from);
+            distance[first][place_count] = distance[place_count][first] = length;
+        }
     } else {
         places[++place_count] = from;
         places[++place_count] = to;
@@ -35,23 +41,27 @@ void record(std::string from, std::string to, int length) {
 }
 
 int Dijkstra(std::string from, std::string to) {
-    int s, t, min, pre;
-    int dis[101];
-    bool visited[101];
+    int s= 0, t = 0, min, pre;
+    int dis[205];
+    bool visited[205];
     memset(visited, false, sizeof(visited));
-    for (int i = 0; i < 101; i++) dis[101] = 10000;
+    for (int i = 0; i < 205; i++) dis[i] = 1000000;
     for (int i = 1; i <= place_count; i++) {
         if (places[i] == from) s = i;
         if (places[i] == to) t = i;
     }
+    if (!s || !t) return -1;
+    dis[s] = 0;
     for (int i = 1; i <= place_count; i++) {
-        int min = 10000, pre = s;
+        int min = 1000000, pre = s;
         for (int j = 1; j <= place_count; j++) {
             if (!visited[j] && dis[j] < min) min = dis[j], pre = j;
         }
         visited[pre] = true;
-        for (int j = 1; i <= place_count; j++) {
-            dis[j] = min(dis[j], dis[pre] + distance[pre][j]);
+        for (int j = 1; j <= place_count; j++) {
+            if (!visited[j]) {
+                dis[j] = min(dis[j], dis[pre] + distance[pre][j]);
+            }
         }
     }
     if (visited[t]) return dis[t];
@@ -62,10 +72,10 @@ int main() {
     int t;
     scanf("%d", &t);
     while (t--) {
-        for (int i = 0; i < 101; i++) places[i] = "";
-        for (int i = 0; i < 101; i++)
-            for (int j = 0; j < 101; j++)
-                distance[i][j] = 10000;
+        for (int i = 0; i < 205; i++) places[i] = "";
+        for (int i = 0; i < 205; i++)
+            for (int j = 0; j < 205; j++)
+                distance[i][j] = 1000000;
         place_count = 0;
         int length;
         std::string from, to;
