d8b7629f2aa83d7e6f43fac2b96268737fed2f98
laiy <ly.franky@gmail.com>
2015-01-22 09:39:36 +0800

    I must enjoy torturing myself

--- extraExercises/1009.cpp
+++ extraExercises/1009.cpp
@@ -1,43 +1,40 @@
 #include <cstdio>
 
-void QuickSort(int a[], int numsize) {
-    int i = 0, j = numsize - 1;
-    int val = a[0];
-    if (numsize > 1) {
-        while (i < j) {
-            for (; j > i; j--) {
-                if (a[j]< val) {
-                    a[i++] = a[j];
-                    break;
-                }
-            }
-            for (; i < j; i++) {
-                if (a[i]> val) {
-                    a[j--] = a[i];
-                    break;
-                }
-            }
-        }
-        a[i] = val;
-        QuickSort(a, i);
-        QuickSort(a + i + 1, numsize - i - 1);
+int a[10];
+
+int partition(int left, int right) {
+    int key = a[left];
+    while (left < right) {
+        while (a[right] >= key && left < right) right--;
+        a[left] = a[right];
+        while (a[left] <= key && left < right) left++;
+        a[right] = a[left];
+    }
+    a[left] = key;
+    return left;
+}
+
+void QuickSort(int left, int right) {
+    if (left < right) {
+        int pivot = partition(left, right);
+        QuickSort(left, pivot - 1);
+        QuickSort(pivot + 1, right);
     }
 }
 
 int main() {
     int n;
     scanf("%d", &n);
-    int signal[10];
     for (int i = 0; i < n; i++)
-        scanf("%d", &signal[i]);
-    QuickSort(signal, n);
+        scanf("%d", &a[i]);
+    QuickSort(0, n - 1);
     for (int i = 0; i < n - 1; i++)
-        printf("%d ", signal[i]);
-    printf("%d\n", signal[n - 1]);
+        printf("%d ", a[i]);
+    printf("%d\n", a[n - 1]);
     if (n % 2 == 0)
-        printf("%d", (signal[n / 2 - 1] + signal[n / 2]) / 2);
+        printf("%d", (a[n / 2 - 1] + a[n / 2]) / 2);
     else
-        printf("%d", signal[n / 2]);
+        printf("%d", a[n / 2]);
     return 0;
 }
 
