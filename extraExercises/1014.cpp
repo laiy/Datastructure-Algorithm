a472e22b3a413605c88d349bfa2cfdc64391a7c5
laiy <ly.franky@gmail.com>
2015-01-22 10:08:35 +0800

    Locating the required gigapixels to render...

--- extraExercises/1014.cpp
+++ extraExercises/1014.cpp
@@ -1,69 +1,6 @@
-/*
- *method1: spfa
- *#include<cstdio>
- *#include<cstring>
- *#include<queue>
- *#define MAXN 2010
- *#define MAXNF 210
- *#define INF 0x3f3f3f3f
- *
- *int n, m, e, a, b, c, s, t;
- *std::queue<int> q;
- *int v[MAXN], d[MAXN], next[MAXN], w[MAXN], first[MAXNF];
- *bool inq[MAXNF];
- *
- *void init() {
- *    e = 0;
- *    memset(first, -1, sizeof(first));
- *}
- *
- *void add_edge(int a, int b, int c) {
- *    v[e] = b;
- *    w[e] = c;
- *    next[e] = first[a];
- *    first[a] = e++;
- *}
- *
- *void spfa(int st) {
- *    memset(d, 0x3f, sizeof(d));
- *    d[st] = 0; 
- *    inq[st] = true;
- *    q.push(st);
- *    while(!q.empty()) {
- *        int u = q.front();
- *        q.pop();
- *        inq[u] = 0;
- *        for(int i = first[u]; i != -1; i = next[i]) {
- *            if(d[v[i]] > d[u]+w[i]) {
- *                d[v[i]] = d[u]+w[i];
- *                if(!inq[v[i]]) q.push(v[i]), inq[v[i]] = true;
- *            }
- *        }
- *    }
- *}
- *
- *int main() {
- *    while(~scanf("%d%d",&n,&m)) {
- *       init();
- *       for(int i=0;i<m;i++) {
- *           scanf("%d%d%d", &a, &b, &c);
- *           add_edge(a, b, c);
- *           add_edge(b, a, c);
- *       }
- *       scanf("%d%d", &s, &t);
- *       spfa(s);
- *       printf("%d\n", d[t] == INF ? -1 : d[t]);
- *    }
- *    return 0;
- *}
- */
-
-/*
- *method2: Dijkstra
- */
-
 #include <stdio.h>
 #define N 10000000
+#define min(a, b) (a < b ? a : b)
 
 int dis[200], map[200][200];
 bool visited[200];
@@ -80,27 +17,16 @@ void init() {
 }
 
 int Dijkstra(int s, int t) {
-    int min, pre = s;
     dis[s] = 0;
-    while (true) {
-        visited[pre] = true;
-        if (pre == t) break;
-        for (int i = 0; i < n; i++) {
-            if (map[pre][i] != N && !visited[i]) {
-                dis[i] = (dis[i] <= dis[pre] + map[pre][i]) ? dis[i] : dis[pre] + map[pre][i];
-            }
-        }
-        min = N;
-        pre = -1;
-        for (int i= 0; i < n; i++) {
-            if (dis[i] < min && !visited[i]) {
-                min = dis[i];
-                pre = i;
-            }
-        }
-        if (pre == -1) break;
+    for (int i = 0; i < n; i++) {
+        int min = N, index;
+        for (int j = 0; j < n; j++)
+            if (!visited[j] && dis[j] < min) min = dis[j], index = j;
+        visited[index] = true;
+        for (int j = 0; j < n; j++)
+            if (!visited[j]) dis[j] = min(dis[j], dis[index] + map[index][j]);
     }
-    return ((pre== -1) ? -1 : dis[pre]);
+    return visited[t] ? dis[t] : -1;
 }
 
 int main() {
