a99c48c70f943363670f13873855505d365604f4
laiy <ly.franky@gmail.com>
2015-09-25 17:01:56 +0800

    removed

--- sicily/1046.c
+++ sicily/1046.c
@@ -1,8 +1,3 @@
-// Problem#: 1046
-// Submission#: 3844923
-// The source code is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License
-// URI: http://creativecommons.org/licenses/by-nc-sa/3.0/
-// All Copyright reserved by Informatic Lab of Sun Yat-sen University
 #include <cstdio>
 #include <vector>
 #include <algorithm>
