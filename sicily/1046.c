c5e6d5db9678affb7dce15c6c34d682470788826
laiy <ly.franky@gmail.com>
2015-09-25 17:01:02 +0800

    0.00s 748kb

---
+++ sicily/1046.c
@@ -0,0 +1,59 @@
+// Problem#: 1046
+// Submission#: 3844923
+// The source code is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License
+// URI: http://creativecommons.org/licenses/by-nc-sa/3.0/
+// All Copyright reserved by Informatic Lab of Sun Yat-sen University
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+struct Plan {
+    short end, length;
+    float avg_planes;
+    Plan(short end, short length, float avg_planes) {
+        this->end = end;
+        this->length = length;
+        this->avg_planes = avg_planes;
+    }
+    bool operator<(const Plan &p) const {
+        if (this->avg_planes != p.avg_planes)
+            return this->avg_planes > p.avg_planes;
+        else
+            return (this->length != p.length) ? this->length > p.length : this->end < p.end;
+    }
+};
+
+int main() {
+    short n, k, numbers_of_quarters, numbers_of_output, minimum_numbers_of_quarters, i, j, t;
+    scanf("%hd", &n);
+    std::vector<Plan> v;
+    float avg_planes;
+    for (k = 1; k <= n; k++) {
+        scanf("%hd %hd %hd", &numbers_of_quarters, &numbers_of_output, &minimum_numbers_of_quarters);
+        short numbers_of_planes[numbers_of_quarters];
+        short record[numbers_of_quarters+ 1][numbers_of_quarters + 1];
+        for (i = 0; i < numbers_of_quarters; i++)
+            scanf("%hd", numbers_of_planes + i);
+        v.clear();
+        for (j = minimum_numbers_of_quarters; j <= numbers_of_quarters; j++)
+            for (i = 0; i <= numbers_of_quarters - j; i++) {
+                avg_planes = 0;
+                if (j == minimum_numbers_of_quarters) {
+                    for (t = 0; t < j; t++)
+                        avg_planes += numbers_of_planes[i + t];
+                    record[i + 1][i + j] = avg_planes;
+                } else {
+                    avg_planes = record[i + 2][i + j] + numbers_of_planes[i];
+                    record[i + 1][i + j] = avg_planes;
+                }
+                avg_planes /= j;
+                v.push_back(Plan(i + j, j, avg_planes));
+            }
+        std::sort(v.begin(), v.end());
+        printf("Result for run %hd:\n", k);
+        for (i = 0; i < numbers_of_output && i < (short)v.size(); i++)
+            printf("%hd-%hd\n", (short)(v[i].end - v[i].length + 1), v[i].end);
+    }
+    return 0;
+}
+
