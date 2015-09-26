018fa99df2d24a58bae7013a864f40d38598c25c
laiy <ly.franky@gmail.com>
2015-09-26 19:48:35 +0800

    DP-bottom to top 0.01s->4088kb run memory

--- sicily/1176.c
+++ sicily/1176.c
@@ -2,30 +2,23 @@
 #include <cstring>
 #define max(a, b) (a > b) ? a : b
 
-int arr[1000];
-int remember_arr[1000][1000];
-
-int dp(int left, int right) {
-    int ans;
-    if (right - left == 1)
-        ans = max(arr[left], arr[right]);
-    else if (remember_arr[left][right] >= 0)
-        ans = remember_arr[left][right];
-    else
-        ans = max(arr[left] + (arr[left + 1] >= arr[right] ? dp(left + 2, right) : dp(left + 1, right - 1)),
-                arr[right] + (arr[left] >= arr[right - 1] ? dp(left + 1, right - 1) : dp(left, right - 2)));
-    return remember_arr[left][right] = ans;
-}
-
 int main() {
     short n, count = 1;
-    int i, sum;
+    int i, sum, j;
     while (scanf("%hd", &n) && n != 0) {
         sum  = 0;
-        memset(remember_arr, -1, sizeof(remember_arr));
+        short arr[n];
+        int dp[n][n];
         for (i = 0; i < n; i++)
-            scanf("%d", arr + i), sum += arr[i];
-        printf("In game %hd, the greedy strategy might lose by as many as %d points.\n", count++, 2 * dp(0, n - 1) - sum);
+            scanf("%hd", arr + i), sum += arr[i];
+        for (i = 0; i < n - 1; i++)
+            dp[i][i + 1] = max(arr[i], arr[i + 1]);
+        if (n > 2)
+            for (i = 4; i <= n; i += 2)
+                for (j = 0; j + i - 1 < n; j++)
+                    dp[j][j + i - 1] = max(arr[j] + (arr[j + 1] >= arr[j + i - 1] ? dp[j + 2][j + i - 1] : dp[j + 1][j + i - 2]),
+                            arr[j + i - 1] + (arr[j] >= arr[j + i - 2] ? dp[j + 1][j + i - 2] : dp[j][j + i - 3]));
+        printf("In game %hd, the greedy strategy might lose by as many as %d points.\n", count++, 2 * dp[0][n - 1] - sum);
     }
     return 0;
 }
