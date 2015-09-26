07371f25120a099efdf0684454617ab13f5e4d13
laiy <ly.franky@gmail.com>
2015-09-26 11:57:26 +0800

    fucking run memory!

---
+++ sicily/1198.c
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+
+bool cmp(std::string a, std::string b) {
+    return a + b < b + a;
+}
+
+int main() {
+    int t, n, i;
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d", &n);
+        std::string s[8];
+        for (i = 0; i < n; i++)
+            std::cin >> s[i];
+        std::sort(s, s + n, cmp);
+        for (i = 0; i < n; i++)
+            std::cout << s[i];
+        printf("\n");
+    }
+    return 0;
+}
