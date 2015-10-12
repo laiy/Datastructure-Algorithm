a517394af12522bdcb71db62bf168079666bf4f7
laiy <ly.franky@gmail.com>
2015-10-12 19:45:16 +0800

    remember the tree -> 0.07s

--- sicily/1151.c
+++ sicily/1151.c
@@ -1,123 +1,72 @@
 #include <cstdio>
-#include <string>
 #include <iostream>
+#include <hash_map>
+#include <hash_set>
+#include <string>
 #include <queue>
-#include <cstring>
 
-int n, destination, top, bottom, a, b, c, d, base, i, j;
-std::string ans;
-int fact[] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320};
-bool visited[40321];
+using namespace __gnu_cxx;
 
-struct State {
-    int state;
+int a[] = {1, 2, 3, 4, 8, 7, 6, 5};
+__gnu_cxx::hash_map<int, std::string> record;
+
+struct Node {
+    int code;
     std::string step;
-    State(int state, std::string step) {
-        this->state = state;
-        this->step = step;
+    Node(int c, std::string s) {
+        this->code = c;
+        this->step = s;
     }
 };
 
-int encode(int n)
-{
-    static int tmp[8];
-    for (i = 7; i >= 0; i--) {
-        tmp[i] = n % 10;
-        n /= 10;
-    }
-
-    for (i = 0; i < 8; i++) {
-        base = 0;
-        for (j = i + 1; j < 8; j++)
-            if (tmp[i] > tmp[j]) base++;
-        n += fact[7 - i] * base;
-    }
-
-    return n;
+inline  int OP_A(int n) {
+    return (n & 4095) << 12 | n >> 12;
 }
 
-int OP_A(int state) {
-    return state / 10000 + state % 10000 * 10000;
+inline int OP_B(int n) {
+    return (( 7 << 9 | 7 << 21 ) & n) >> 9 | (~( 7 << 9 | 7 << 21 ) & n) << 3;
 }
 
-int OP_B(int state) {
-    top = state / 10000;
-    bottom = state % 10000;
-    top = top % 10 * 1000 + top / 10;
-    bottom = bottom % 10 * 1000 + bottom / 10;
-    return top * 10000 + bottom;
+inline int OP_C(int n) {
+    return ((7 | 7 << 9 | 7 << 12 | 7 << 21) & n) | ((7 << 3) & n) << 3 | ((7 << 6) & n) << 12 | ((7 << 15) & n) >> 12 | ((7 << 18) & n) >> 3;
 }
 
-int OP_C(int state) {
-    a = state / 1000000 % 10;
-    b = state / 100000 % 10;
-    c = state / 100 % 10;
-    d = state / 10 % 10;
-
-    state -= a * 1000000;
-    state -= b * 100000;
-    state -= c * 100;
-    state -= d * 10;
-
-    state += c * 1000000;
-    state += a * 100000;
-    state += d * 100;
-    state += b * 10;
-
-    return state;
+inline int zip(int *a) {
+    int code = 0;
+    for (int i = 0; i < 8; ++i)
+        code |= (a[i] - 1) << (3 * i);
+    return  code;
 }
 
 void bfs() {
-    int state = 12348765;
-    if (state == destination) {
-        ans = "";
-        return;
-    }
-
-    std::queue<State> q;
-    q.push(State(state, ""));
+    std::queue<Node> q;
+    q.push(Node(zip(a), ""));
+    __gnu_cxx::hash_set<int> visited;
     while (!q.empty()) {
-        State s = q.front();
+        Node temp = q.front();
         q.pop();
-
-        if (visited[encode(s.state)])
+        if (visited.find(temp.code) != visited.end())
             continue;
-        if (s.step.length() > (size_t)n)
-            break;
-        if (s.state == destination) {
-            ans = s.step;
-            break;
-        }
-
-        visited[encode(s.state)] = true;
-
-        q.push(State(OP_A(s.state), s.step + "A"));
-        q.push(State(OP_B(s.state), s.step + "B"));
-        q.push(State(OP_C(s.state), s.step + "C"));
+        visited.insert(temp.code);
+        record[temp.code] = temp.step;
+        q.push(Node(OP_A(temp.code), temp.step + "A"));
+        q.push(Node(OP_B(temp.code), temp.step + "B"));
+        q.push(Node(OP_C(temp.code), temp.step + "C"));
     }
 }
 
 int main() {
+    bfs();
+    int n, arr[8], i, code;
     while (scanf("%d", &n) && n != -1) {
-
-        ans = "NOT FOUND";
-        destination = 0;
-        memset(visited, 0, sizeof(visited));
-
-        int temp[8], base = 1, i;
         for (i = 0; i < 8; i++)
-            scanf("%d", temp + i);
-        for (i = 7; i >= 0; i--)
-            destination += temp[i] * base, base *= 10;
-
-        bfs();
-
-        if (ans == "NOT FOUND")
-            printf("-1\n");
+            scanf("%d", arr + i);
+        code = zip(arr);
+        if (record[code].length() <= (size_t)n)
+            std::cout << record[code].length() << " " << record[code] << std::endl;
         else
-            std::cout << ans.length() << " " << ans << std::endl;
+            printf("-1\n");
     }
     return 0;
 }
-
 
