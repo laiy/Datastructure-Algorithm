af3a112f72fbdb5110a055355fa0e7b2099e068a
laiy <ly.franky@gmail.com>
2015-11-20 21:05:03 +0800

    matrix

--- sicily/1211.c
+++ sicily/1211.c
@@ -2,24 +2,39 @@
 #include <cstring>
 
 int main() {
-    int n, m, l, i, from, to, query, a, b, k, j, x;
+    int n, m, l, i, from, to, query, a, b, k, j, x, time;
     scanf("%d %d %d", &n, &m, &l);
-    bool road[n + 1][n + 1];
-    int dp[n + 1][n + 1][l + 1];
+    int road[n + 1][n + 1];
+    int copy[n + 1][n + 1];
+    int temp_result[n + 1][n + 1];
     memset(road, 0, sizeof(road));
-    memset(dp, 0, sizeof(dp));
     for (i = 0; i < m; i++)
-        scanf("%d %d", &from, &to), road[from][to] = true, dp[from][to][1] = 1;
-    for (k = 2; k <= l; k++)
-        for (i = 1; i <= n; i++)
-            for (j = 1; j <= n; j++)
-                for (x = 1; x <= n; x++)
-                    if (road[i][x])
-                        dp[i][j][k] += dp[x][j][k - 1];
+        scanf("%d %d", &from, &to), road[from][to]++;
+    memcpy(copy, road, sizeof(road));
+    time = 1;
+    while (time * 2 <= l) {
+        for (k = 1; k <= n; k++)
+            for (i = 1; i <= n; i++) {
+                temp_result[i][k] = 0;
+                for (j = 1; j <= n; j++)
+                    temp_result[i][k] += road[i][j] * road[j][k];
+            }
+        memcpy(road, temp_result, sizeof(temp_result));
+        time *= 2;
+    }
+    for (x = 1; x <= l - time; x++) {
+        for (k = 1; k <= n; k++)
+            for (i = 1; i <= n; i++) {
+                temp_result[i][k] = 0;
+                for (j = 1; j <= n; j++)
+                    temp_result[i][k] += road[i][j] * copy[j][k];
+            }
+        memcpy(road, temp_result, sizeof(temp_result));
+    }
     scanf("%d", &query);
     while (query--) {
         scanf("%d %d", &a, &b);
-        printf("%d\n", dp[a][b][l]);
+        printf("%d\n", road[a][b]);
     }
 }
 
