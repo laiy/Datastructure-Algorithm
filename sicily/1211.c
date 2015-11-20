b723a897fb311a328e18c29ae5361fdcb6fba5b4
laiy <ly.franky@gmail.com>
2015-11-20 16:38:55 +0800

    dp 0.35s

--- sicily/1211.c
+++ sicily/1211.c
@@ -11,32 +11,24 @@ struct Node {
 };
 
 int main() {
-    int n, m, l, i, from, to, query, a, b, sum;
+    int n, m, l, i, from, to, query, a, b, k, j, x;
     scanf("%d %d %d", &n, &m, &l);
     bool road[n + 1][n + 1];
+    int dp[n + 1][n + 1][l + 1];
     memset(road, 0, sizeof(road));
+    memset(dp, 0, sizeof(dp));
     for (i = 0; i < m; i++)
-        scanf("%d %d", &from, &to), road[from][to] = true;
+        scanf("%d %d", &from, &to), road[from][to] = true, dp[from][to][1] = 1;
+    for (k = 2; k <= l; k++)
+        for (i = 1; i <= n; i++)
+            for (j = 1; j <= n; j++)
+                for (x = 1; x <= n; x++)
+                    if (road[i][x])
+                        dp[i][j][k] += dp[x][j][k - 1];
     scanf("%d", &query);
     while (query--) {
         scanf("%d %d", &a, &b);
-        std::queue<Node> q;
-        q.push(Node(a, 0));
-        sum = 0;
-        while (!q.empty()) {
-            Node temp = q.front();
-            q.pop();
-            if (temp.step > l)
-                break;
-            if (temp.step == l && temp.location == b)
-                sum++;
-            else {
-                for (i = 1; i <= n; i++)
-                    if (road[temp.location][i])
-                        q.push(Node(i, temp.step + 1));
-            }
-        }
-        printf("%d\n", sum);
+        printf("%d\n", dp[a][b][l]);
     }
 }
 
