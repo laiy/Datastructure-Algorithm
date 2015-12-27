0ce7abe9856593d014c4cb860bc747a651bbd92c
laiy <ly.franky@gmail.com>
2015-12-27 18:14:24 +0800

    pep8 - cause I fell like doing a barrel roll

---
+++ sicily/1099.c
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <cmath>
+
+inline long long gcd_extend(int &a, int b, long long *x, long long *y) {
+    static long long r, t;
+    if (b == 0) {
+        *x = 1, *y = 0;
+        return a;
+    } else {
+        r = gcd_extend(b, a % b, x, y);
+        t = *x;
+        *x = *y;
+        *y = t - a / b * *y;
+        return r;
+    }
+}
+
+int main() {
+    int passengers;
+    int cost_A, passenger_A, cost_B, passenger_B;
+    int count = 1;
+    long long lower, upper, t, gcd, x, y;
+    while (scanf("%d", &passengers) && passengers) {
+        scanf("%d %d", &cost_A, &passenger_A);
+        scanf("%d %d", &cost_B, &passenger_B);
+        gcd = gcd_extend(passenger_A, passenger_B, &x, &y);
+        if (passengers % gcd == 0) {
+            x *= passengers / gcd;
+            y *= passengers / gcd;
+            upper = floor((double)y / (passenger_A / gcd));
+            lower = ceil((double)-x / (passenger_B / gcd));
+            t = passenger_B * cost_A - passenger_A * cost_B <= 0 ? upper : lower;
+            printf("Data set %d: %lld aircraft A, %lld aircraft B\n", count++, x + (passenger_B / gcd) * t, y - (passenger_A / gcd) * t);
+        } else
+            printf("Data set %d: cannot be flown\n", count++);
+    }
+    return 0;
+}
+
