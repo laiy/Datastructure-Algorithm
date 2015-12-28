e1a7a6c37d7c9b8f80df3a4f2e57837e2a5c143d
laiy <ly.franky@gmail.com>
2015-12-28 19:36:49 +0800

    Shit code!

--- sicily/1119.c
+++ sicily/1119.c
@@ -1,17 +1,24 @@
 #include <cstdio>
 #include <cmath>
+#include <hash_map>
 
 int main() {
-    int year, bits, n;
-    double threshold, sum;
-    while (scanf("%d", &year) && year) {
-        bits = pow(2, (year - 1960) / 10 + 2);
-        threshold = bits * log(2);
-        sum = 0, n = 1;
-        while (sum <= threshold)
-            sum += log(n++);
-        printf("%d\n", n - 2);
+    int year, n;
+    double threshold, sum, log2 = log(2);
+    __gnu_cxx::hash_map<int, int> table;
+    double sum_table[254019];
+    sum = 0;
+    for (n = 1; n <= 254018; n++)
+        sum += log(n), sum_table[n] = sum;
+    n = 3;
+    for (year = 1960; year <= 2160; year += 10) {
+        threshold = pow(2, year / 10 - 194) * log2;
+        while (sum_table[n++] <= threshold)
+            ;
+        table[year] = n - 2;
     }
+    while (scanf("%d", &year) && year)
+        printf("%d\n", table[year - year % 10]);
     return 0;
 }
 
