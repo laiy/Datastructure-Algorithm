347fdab94f63a07eec95fe706a4ac8419966ccd2
laiy <ly.franky@gmail.com>
2015-12-29 17:52:27 +0800

    Well the book was obviously wrong.

--- sicily/1317.c
+++ sicily/1317.c
@@ -11,20 +11,23 @@ struct Node {
         this->j = j;
         this->weight = weight;
     }
+    Node(const Node &n) {
+        this->i = n.i;
+        this->j = n.j;
+        this->weight = n.weight;
+    }
+    bool operator<(const Node &n) const {
+        return this->weight < n.weight;
+    }
 };
 
-std::vector<Node*> nodes;
+std::vector<Node> nodes;
 
 short board[10][10];
-Node *map[10][10];
 bool col_space[10][10], row_space[10][10], block_space[3][3][10];
 int solutions;
 short record[10][10];
 bool exist[10][10];
-
-bool cmp(const Node* a, const Node* b) {
-    return a->weight < b->weight;
-}
 
 inline void update_weight(int i, int j) {
     if (exist[i][j])
@@ -33,7 +36,9 @@ inline void update_weight(int i, int j) {
     for (k = 1; k < 10; k++)
         if (row_space[i][k] || col_space[j][k] || block_space[(i - 1) / 3][(j - 1) / 3][k])
             weight--;
-    map[i][j]->weight = weight;
+    for (k = 0; (size_t)k < nodes.size(); k++)
+        if (nodes[k].i == i && nodes[k].j == j)
+            nodes[k].weight = weight;
 }
 
 inline void count_weight(int i, int j) {
@@ -50,7 +55,6 @@ inline void count_weight(int i, int j) {
 }
 
 void heuristic_dfs() {
-    /* printf("%d\n", (int)nodes.size()); */
     if (nodes.empty()) {
         if (!solutions)
             memcpy(record, board, sizeof(board));
@@ -58,61 +62,58 @@ void heuristic_dfs() {
         return;
     }
     int k;
-    std::sort(nodes.begin(), nodes.end(), cmp);
-    printf("%d\n", nodes[0]->weight);
+    std::sort(nodes.begin(), nodes.end());
+    Node temp = Node(nodes[0]);
+    nodes.erase(nodes.begin());
     std::queue<int> q;
     for (k = 1; k < 10; k++)
-        if (!row_space[nodes[0]->i][k] && !col_space[nodes[0]->j][k] && !block_space[(nodes[0]->i - 1) / 3][(nodes[0]->j - 1) / 3][k])
+        if (!(row_space[temp.i][k] || col_space[temp.j][k] || block_space[(temp.i - 1) / 3][(temp.j - 1) / 3][k]))
             q.push(k);
     while (!q.empty()) {
         k = q.front();
         q.pop();
 
-        row_space[nodes[0]->i][k] = true;
-        col_space[nodes[0]->j][k] = true;
-        block_space[(nodes[0]->i - 1) / 3][(nodes[0]->j - 1) / 3][k] = true;
-
-        count_weight(nodes[0]->i, nodes[0]->j);
-
-        /* printf("i: %d j: %d k: %d\n", nodes[0]->i, nodes[0]->j, k); */
-
-        board[nodes[0]->i][nodes[0]->j] = k;
-        Node* temp = nodes[0];
-        nodes.erase(nodes.begin());
-
+        row_space[temp.i][k] = true;
+        col_space[temp.j][k] = true;
+        block_space[(temp.i - 1) / 3][(temp.j - 1) / 3][k] = true;
+        count_weight(temp.i, temp.j);
+        board[temp.i][temp.j] = k;
         heuristic_dfs();
-
-        nodes.insert(nodes.begin(), temp);
-        board[nodes[0]->i][nodes[0]->j] = -1;
-
-        row_space[nodes[0]->i][k] = false;
-        col_space[nodes[0]->j][k] = false;
-        block_space[(nodes[0]->i - 1) / 3][(nodes[0]->j - 1) / 3][k] = false;
-
-        count_weight(nodes[0]->i, nodes[0]->j);
+        board[temp.i][temp.j] = -1;
+        row_space[temp.i][k] = false;
+        col_space[temp.j][k] = false;
+        block_space[(temp.i - 1) / 3][(temp.j - 1) / 3][k] = false;
+        count_weight(temp.i, temp.j);
     }
+    nodes.push_back(temp);
 }
 
 int main() {
     int t, i, j, k, weight, count = 1;
     scanf("%d", &t);
-    char input;
+    char input[10];
+    bool fuck = false;
     while (t--) {
+        if (!fuck) {
+            fuck = true;
+        } else {
+            printf("\n");
+        }
         memset(board, -1, sizeof(board));
         memset(col_space, 0, sizeof(col_space));
         memset(row_space, 0, sizeof(row_space));
         memset(block_space, 0, sizeof(block_space));
         memset(exist, 0, sizeof(exist));
-        memset(map, 0, sizeof(map));
         solutions = 0;
         nodes.clear();
-        for (i = 1; i <= 9; i++)
-            for (j = 1; j <= 9; j++) {
-                scanf("%c", &input);
-                if (input != '_')
-                    board[i][j] = input - '0', row_space[i][board[i][j]] = true, col_space[j][board[i][j]] = true, \
-                        block_space[(i - 1) / 3][(j - 1) / 3][board[i][j]] = true;
+        for (i = 1; i <= 9; i++) {
+            scanf("%s", input);
+            for (j = 0; j < 9; j++) {
+                if (input[j] != '_')
+                    board[i][j + 1] = input[j] - '0', row_space[i][board[i][j + 1]] = true, col_space[j + 1][board[i][j + 1]] = true, \
+                        block_space[(i - 1) / 3][(j) / 3][board[i][j + 1]] = true;
             }
+        }
         for (i = 1; i <= 9; i++)
             for (j = 1; j <= 9; j++)
                 if (board[i][j] == -1) {
@@ -120,9 +121,7 @@ int main() {
                     for (k = 1; k < 10; k++)
                         if (row_space[i][k] || col_space[j][k] || block_space[(i - 1) / 3][(j - 1) / 3][k])
                             weight--;
-                    Node node = Node(i, j, weight);
-                    map[i][j] = &node;
-                    nodes.push_back(&node);
+                    nodes.push_back(Node(i, j, weight));
                 } else
                     exist[i][j] = true;
         heuristic_dfs();
@@ -138,7 +137,6 @@ int main() {
                 printf("\n");
             }
         }
-        printf("\n");
     }
     return 0;
 }
