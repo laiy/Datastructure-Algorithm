686b4f29e7ff4cd3cf34b358523eafb1e3cf17e7
laiy <ly.franky@gmail.com>
2016-01-11 17:21:47 +0800

    this doesn't really make things faster, but I tried

--- sicily/1215.c
+++ sicily/1215.c
@@ -1,87 +1,29 @@
-#include <cstdio>
-#include <cstring>
+#include <iostream>
 #include <queue>
-
-char location_map[5];
-int move_x[4];
-int move_y[4];
-int direction_x[4] = {-1, 1, 0, 0};
-int direction_y[4] = {0, 0, -1, 1};
-int n, m, steps;
-bool impossible = false;
-bool pre_meet = false;
-char map[21][21];
-int threshold = 256;
+#include <cstring>
+#include <cstdio>
 
 struct State {
-    int h_position_x, h_position_y, p_position_x, p_position_y, steps;
-    State(int h_position_x, int h_position_y, int p_position_x, int p_position_y, int steps) {
-        this->h_position_x = h_position_x;
-        this->h_position_y = h_position_y;
-        this->p_position_x = p_position_x;
-        this->p_position_y = p_position_y;
-        this->steps = steps;
-    }
-};
-
-inline bool meet(int p_position_x, int p_position_y, int h_position_x, int h_position_y, int possible_step) {
-    if (p_position_x == h_position_x && p_position_y == h_position_y)
-        return true;
-    if (p_position_x + 1 == h_position_x && p_position_y == h_position_y && location_map[1] == 'N')
-        threshold = possible_step;
-    if (p_position_x - 1 == h_position_x && p_position_y == h_position_y && location_map[0] == 'S')
-        threshold = possible_step;
-    if (p_position_x == h_position_x && p_position_y + 1 == h_position_y && location_map[3] == 'W')
-        threshold = possible_step;
-    if (p_position_x == h_position_x && p_position_y - 1 == h_position_y && location_map[2] == 'E')
-        threshold = possible_step;
-    return false;
-}
-
-inline bool valid_pos(int x, int y) {
-    return x >= 0 && x < n && y >= 0 && y < m;
-}
-
-void bfs(State original_state) {
-    std::queue<State> q;
-    q.push(original_state);
-    int px, py, hx, hy;
-    while (!q.empty()) {
-        State temp = q.front();
-        q.pop();
-        if (temp.steps > 255) {
-            impossible = true;
-            return;
-        }
-        if (temp.steps == threshold) {
-            steps = threshold;
-            return;
-        }
-        if (meet(temp.p_position_x, temp.p_position_y, temp.h_position_x, temp.h_position_y, temp.steps + 1)) {
-            steps = temp.steps;
-            return;
-        }
-        for (int i = 0; i < 4; i++) {
-            px = temp.p_position_x + direction_x[i];
-            py = temp.p_position_y + direction_y[i];
-            hx = temp.h_position_x + move_x[i];
-            hy = temp.h_position_y + move_y[i];
-            if (valid_pos(px, py) && map[px][py] == '.') {
-                if (map[hx][hy] == '#')
-                    q.push(State(temp.h_position_x, temp.h_position_y, px, py, temp.steps + 1));
-                else if (map[hx][hy] == '.')
-                    q.push(State(hx, hy, px, py, temp.steps + 1));
-            }
-        }
-    }
-}
+    short px, py, hx, hy, step;
+} temp, new_state;
 
 int main() {
-    scanf("%d %d", &n, &m);
-    int i, j, h_position_x, h_position_y, p_position_x, p_position_y;
-    char row[21];
+    short n, m, i, j;
+    short px, py, hx, hy;
+    char map[20][20];
+    bool visited[20][20][20][20], flag = true;
+    char location_map[4];
+    short move_x[4], move_y[4], direction_x[4] = {-1, 1, 0, 0}, direction_y[4] = {0, 0, -1, 1};
+    memset(visited, 0, sizeof(visited));
+    scanf("%hd %hd", &n, &m);
     for (i = 0; i < n; i++)
-        scanf("%s", row), memcpy(&map[i], &row[0], sizeof(strlen(row)));
+        for (j = 0; j < m; j++) {
+            std::cin >> map[i][j];
+            if (map[i][j] == 'P')
+                temp.px = i, temp.py = j;
+            if (map[i][j] == 'H')
+                temp.hx = i, temp.hy = j;
+        }
     scanf("%s", location_map);
     for (i = 0; i < 4; i++)
         if (location_map[i] == 'N')
@@ -92,17 +34,37 @@ int main() {
             move_x[i] = 0, move_y[i] = -1;
         else if (location_map[i] == 'E')
             move_x[i] = 0, move_y[i] = 1;
-    for (i = 0; i < n; i++)
-        for (j = 0; j < m; j++)
-            if (map[i][j] == 'H')
-                h_position_x = i, h_position_y = j, map[i][j] = '.';
-            else if (map[i][j] == 'P')
-                p_position_x = i, p_position_y = j, map[i][j] = '.';
-    bfs(State(h_position_x, h_position_y, p_position_x, p_position_y, 0));
-    if (impossible)
+    temp.step = 0;
+    std::queue<State> q;
+    q.push(temp);
+    visited[temp.px][temp.py][temp.hx][temp.hy] = true;
+    while (flag && !q.empty() && q.front().step < 255) {
+        temp = q.front();
+        q.pop();
+        for (i = 0; i < 4; i++) {
+            px = temp.px + direction_x[i];
+            py = temp.py + direction_y[i];
+            hx = temp.hx + move_x[i];
+            hy = temp.hy + move_y[i];
+            if (map[px][py] == '#' || map[px][py] == '!' || map[hx][hy] == '!')
+                continue;
+            if (map[hx][hy] == '#')
+                hx = temp.hx, hy = temp.hy;
+            if (visited[px][py][hx][hy])
+                continue;
+            visited[px][py][hx][hy] = true;
+            new_state.step = temp.step + 1;
+            new_state.px = px, new_state.py = py, new_state.hx = hx, new_state.hy = hy;
+            if ((px == hx && py == hy) || (px == temp.hx && py == temp.hy && hx == temp.px && hy == temp.py)) {
+                printf("%d\n", new_state.step);
+                flag = false;
+                break;
+            }
+            q.push(new_state);
+        }
+    }
+    if (flag)
         printf("Impossible\n");
-    else
-        printf("%d\n", steps);
     return 0;
 }
 
